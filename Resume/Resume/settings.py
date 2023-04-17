3eb52d926feb87a8de5cecbd5edbb69518be280a
Harish <g7harish@gmail.com>
2023-04-17 22:47:08 +0530

    changed

--- Resume/Resume/settings.py
+++ Resume/Resume/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2q(ig!s=l%=ta14s1ewr-l-q5+$fa02@!v4xcv3g#49*x$oug&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app','.now.sh']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
