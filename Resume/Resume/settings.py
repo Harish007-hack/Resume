f1d6616beb782648f64a8f4d6f99d3109af7456b
Harish <g7harish@gmail.com>
2023-05-15 15:56:13 +0530

    updated changes

--- Resume/Resume/settings.py
+++ Resume/Resume/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'Resume.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -121,6 +121,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_URL = 'images/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
